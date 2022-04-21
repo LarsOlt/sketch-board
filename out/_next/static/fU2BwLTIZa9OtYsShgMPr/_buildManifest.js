2bb538c867688167a6927468b7fa87d8d564caa1
Lars Oeltjenbruns <32353679+LarsOlt@users.noreply.github.com>
2022-04-21 17:06:28 +0200

    chore: add build for github pages

---
+++ out/_next/static/fU2BwLTIZa9OtYsShgMPr/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/275-991dd269d954534e.js","static/css/b82117018dc941e0.css","static/chunks/pages/index-d3cc30feb773c7be.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
