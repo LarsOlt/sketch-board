6201bffbc88365c22f8921d2dd94974ee9ee6908
Lars Oeltjenbruns <32353679+LarsOlt@users.noreply.github.com>
2022-04-26 12:56:59 +0200

    fix: nextjs assets not loading

--- next.config.js
+++ next.config.js
@@ -3,8 +3,8 @@ const isProd = process.env.NODE_ENV === "production";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // assetPrefix: "/sketch-board/",
-  // basePath: "/sketch-board",
+  assetPrefix: "/sketch-board/",
+  basePath: "/sketch-board",
 };
 
 module.exports = nextConfig;
